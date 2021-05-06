e3c5f89563adc01b158574c5cb098a519dd726ef
hongphuc-nguyen <83260953+hongphuc-nguyen@users.noreply.github.com>
2021-05-06 11:05:38 +0700

    Add files via upload

---
+++ a.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+name=st.text_input('What is your name?')
+selectbox = st.selectbox('What is your favorite food?',['Hambuger','Sandwich','Susi'])
+
+st.write('Hello ' + name + '!. Nice to meet you!')
