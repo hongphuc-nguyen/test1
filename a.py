2af4613d36eed561bfc898d5f5695b9de72626ea
hongphuc-nguyen <83260953+hongphuc-nguyen@users.noreply.github.com>
2021-05-12 09:44:10 +0700

    Update a.py

--- a.py
+++ a.py
@@ -3,4 +3,5 @@ import streamlit as st
 name=st.text_input('What is your name?')
 selectbox = st.selectbox('What is your favorite food?',['Hambuger','Sandwich','Susi'])
 
-st.write('Hello ' + name + '!. Nice to meet you!')
+st.write('Hello ' + name + '! Nice to meet you!')
+st.write('Hola ' + name + '! Mucho gusto!')
